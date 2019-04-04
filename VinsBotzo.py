92fc6c67cd1489dea135f849f61d87e73dcd8b44
Vincent Vos <vincent.vos@student.fontys.nl>
2019-04-04 13:47:52 +0200

    added version info on_ready

--- VinsBotzo.py
+++ VinsBotzo.py
@@ -23,6 +23,7 @@ async def on_ready():
 	print(client.user.name)
 	print(client.user.id)
 	print('------')
+	print(discord.__version__)
 	await client.change_presence(game=discord.Game(name="Making a bot"))
 
 @client.event 
