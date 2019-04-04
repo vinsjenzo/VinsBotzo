5b9ad85dc4b26cd3636d7998726cc958ec2dcf7a
Vincent Vos <vincent.vos@student.fontys.nl>
2019-04-04 15:15:32 +0200

    jemoeder

--- VinsBotzo.py
+++ VinsBotzo.py
@@ -19,38 +19,46 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-	print('Logged in as')
-	print(client.user.name)
-	print(client.user.id)
-	print('------')
-	print(f"""Discord.py version {discord.__version__}""")
-	await client.change_presence(game=discord.Game(name="Making a bot"))
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    print(f"""Discord.py version {discord.__version__}""")
+    await client.change_presence(game=discord.Game(name="Making a bot"))
+    
+@client.event
+async def on_server_join(server):
+    channel = discord.utils.get(server.channels, name='Status Reports')
+    if channel:
+        return
+    else:
+        await client.create_channel(server, 'Status Reports', type=discord.ChannelType.text)
 
 @client.event 
 async def on_typing(channel, user, when):
-	await client.change_presence(game=discord.Game(name=random.choice(gameNames)))
+    await client.change_presence(game=discord.Game(name=random.choice(gameNames)))
 	
 @client.event
 async def on_message(message):
-	if message.author == client.user:
-		return
-	if "mij niet bellen" in message.content.lower():
-		await client.send_message(message.channel, "OK BRUUR :telephone:")
+    if message.author == client.user:
+        return
+    if "mij niet bellen" in message.content.lower():
+        await client.send_message(message.channel, "OK BRUUR :telephone:")
 
 @client.event
 async def on_message_delete(message):
-	if message.author == client.user:
-		return
-	else:
-		await client.send_message(message.channel, "%s said: %s" %(message.author, message.content))
+    if message.author == client.user:
+        return
+    else:
+        await client.send_message(message.channel, "%s said: %s" %(message.author, message.content))
 
 @client.event
 async def on_member_update(before, after):
-	#if before.status == offline & after.status == online:
-	channel = discord.utils.get(client.get_all_channels(), name='general')
-	if channel:
-		print('{}: {}'.format(channel.name, channel.id))
-		fmt = '{0.mention} is now {0.status}.'
-		await client.send_message(channel, fmt.format(after))
+    #if before.status == offline & after.status == online:
+    channel = discord.utils.get(after.server.channels, name='Status Reports')
+    if channel:
+        print('{}: {}'.format(channel.name, channel.id))
+        fmt = '{0.mention} is now {0.status}.'
+        await client.send_message(channel, fmt.format(after))
 
 client.run(access_token)
