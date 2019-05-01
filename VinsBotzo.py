e68787e419e21060a88a4747c4803e9d7489baca
Vincent Vos <vincent.vos@student.fontys.nl>
2019-05-01 18:19:16 +0200

    removed annoying status update shit

--- VinsBotzo.py
+++ VinsBotzo.py
@@ -26,13 +26,13 @@ async def on_ready():
     print(f"""Discord.py version {discord.__version__}""")
     await client.change_presence(game=discord.Game(name="Making a bot"))
     
-@client.event
-async def on_server_join(server):
-    channel = discord.utils.get(server.channels, name='status-reports')
-    if channel:
-        return
-    else:
-        await client.create_channel(server, 'status-reports', type=discord.ChannelType.text)
+##@client.event
+##async def on_server_join(server):
+##    channel = discord.utils.get(server.channels, name='status-reports')
+##    if channel:
+##        return
+##    else:
+##        await client.create_channel(server, 'status-reports', type=discord.ChannelType.text)
 
 @client.event 
 async def on_typing(channel, user, when):
@@ -44,21 +44,26 @@ async def on_message(message):
         return
     if "mij niet bellen" in message.content.lower():
         await client.send_message(message.channel, "OK BRUUR :telephone:")
+    if message.author.id == "390555869273915394":
+        await client.send_message(message.channel, "Kk bek houde rick!")
+    if message.author.id == "250738667772051460":
+        await client.send_message(message.channel, "Diëgo ga mannen pijpen, pijpsletje!")
+    
 
-@client.event
-async def on_message_delete(message):
-    if message.author == client.user:
-        return
-    else:
-        await client.send_message(message.channel, "%s said: %s" %(message.author, message.content))
-
-@client.event
-async def on_member_update(before, after):
-    #if before.status == offline & after.status == online:
-    channel = discord.utils.get(after.server.channels, name='status-reports')
-    if channel:
-        print('{}: {}'.format(channel.name, channel.id))
-        fmt = '{0.mention} is now {0.status}.'
-        await client.send_message(channel, fmt.format(after))
+##@client.event
+##async def on_message_delete(message):
+##    if message.author == client.user:
+##        return
+##    else:
+##        await client.send_message(message.channel, "%s said: %s" %(message.author, message.content))
+##
+##@client.event
+##async def on_member_update(before, after):
+##    #if before.status == offline & after.status == online:
+##    channel = discord.utils.get(after.server.channels, name='status-reports')
+##    if channel:
+##        print('{}: {}'.format(channel.name, channel.id))
+##        fmt = '{0.mention} is now {0.status}.'
+##        await client.send_message(channel, fmt.format(after))
 
 client.run(access_token)
